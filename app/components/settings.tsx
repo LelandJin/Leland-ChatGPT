5b5c3be03b86005d250b7cd8704e2a78c647f8fe
LelandJin <lnjsz@qq.com>
2023-11-17 00:29:55 +0000

    Set default language to CN

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -1054,7 +1054,7 @@ export function Settings() {
             </>
           )}
 
-          {!shouldHideBalanceQuery ? (
+          {!shouldHideBalanceQuery && !clientConfig?.isApp ? (
             <ListItem
               title={Locale.Settings.Usage.Title}
               subTitle={
